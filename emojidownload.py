688671c8ff914b4f79b8c5b126c48d0c9e095c5d
BigBull21 <arhino98@gmail.com>
2023-09-11 20:01:23 -0400

    Colors

--- emojidownload.py
+++ emojidownload.py
@@ -7,14 +7,14 @@ import csv
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-#for newer versions of chrome, replace with your version of chromedriver
+# for newer versions of chrome, replace with your version of chromedriver
 service = Service(executable_path='./chromedriver.exe')
 
 options = Options()
 options.add_argument('--headless')
 
 # you can replace this line with `driver = webdriver.Chrome(options=options)` if using chromedriver-autoinstaller
-driver = webdriver.Chrome(service= service, options=options)
+driver = webdriver.Chrome(service=service, options=options)
 
 
 driver.get('https://googlefonts.github.io/noto-emoji-animation/')
@@ -27,10 +27,10 @@ soup = BeautifulSoup(page, "lxml")
 result = soup.find('body')
 result = result.find('icons-app')
 result = result.find('mat-sidenav-container')
-elements = result.find_all('button', class_ = "is-svg ng-star-inserted")
+elements = result.find_all('button', class_="is-svg ng-star-inserted")
 
-file = open('emoji-lotties.csv', 'w')
-fields = ["name", "svg","lottie"]
+file = open('emoji.csv', 'w')
+fields = ["name", "svg", "lottie"]
 
 writer = csv.writer(file, lineterminator='\n')
 
@@ -38,35 +38,54 @@ print(f"Opening file... ")
 writer.writerow(fields)
 
 output_folder_svg = 'svg_files'
-os.makedirs(output_folder, exist_ok=True)
+output_folder_lottie = 'lottie_files'
+os.makedirs(output_folder_svg, exist_ok=True)
+os.makedirs(output_folder_lottie, exist_ok=True)
 
-for number,element in enumerate(elements):
+for number, element in enumerate(elements):
     src = element.find('img')
     label = src.get('src')
     location = label.strip()
-    location = location [47:]
+    location = location[47:]
     index = location.find('/')
     if (index >= 0):
-        location = location[0:index];
-    name = element.find('span', class_ = 'icon-name mat-caption')
+        location = location[0:index]
+    name = element.find('span', class_='icon-name mat-caption')
     print(f"[{number +1}/{len(elements)}] {name.text.strip()}: ")
-    lottie = 'https://fonts.gstatic.com/s/e/notoemoji/latest/{}/lottie.json'.format(location)
+    lottie = 'https://fonts.gstatic.com/s/e/notoemoji/latest/{}/lottie.json'.format(
+        location)
     print(f"Writing {name.text.strip()} lottie: {lottie}...")
-    svg = 'https://fonts.gstatic.com/s/e/notoemoji/latest/{}/emoji.svg'.format(location)
+    svg = 'https://fonts.gstatic.com/s/e/notoemoji/latest/{}/emoji.svg'.format(
+        location)
     print(f"Writing {name.text.strip()} svg: {svg}...")
     r = requests.get(svg, allow_redirects=True)
+    print(f"{location[-2:]}")
+    if (location[-2:] == "fb"):
+        name.append('-1')
+    if (location[-2:] == "fc"):
+        name.append('-2')
+    if (location[-2:] == "fd"):
+        name.append('-3')
+    if (location[-2:] == "fe"):
+        name.append('-4')
+    if (location[-2:] == "ff"):
+        name.append('-5')
 
 ################################################################
-# Uncomment the following line to download the json files to directory of script
-    #open('{}.svg'.format(name.text.strip()), 'wb').write(r.content)
+# Uncomment the following lines to download the lottie and SVG files to directory of script
+    lottie_filename = os.path.join(
+        output_folder_lottie, '{}.json'.format(name.text.strip()))
+    open(lottie_filename, 'wb').write(r.content)
+    svg_filename = os.path.join(
+        output_folder_svg, '{}.svg'.format(name.text.strip()))
+    # print(f"Saving {name.text.strip()}")
+    open(svg_filename, 'wb').write(r.content)
+    # print(f"Saved {name.text.strip()}")
+    # print(f"{name.text.strip()} Successfully Scraped.")
 ################################################################
 
     writer.writerow(["{}.json".format(name.text.strip()), svg, lottie])
-    print(f"{name.text.strip()} Successfully Scraped.")
-    print(f"Saving {name.text.strip()}")
-    svg_filename = os.path.join(output_folder, '{}.svg'.format(name.text.strip()))
-    open(svg_filename, 'wb').write(r.content)
-    print(f"Saved {name.text.strip()}")
+
 
 print(f"Closing file ... ")
 file.close()
